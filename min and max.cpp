e0c6132c546998faf1f929613ce088dfa4745bfa
Jay Paygude <jaypaygude2@gmail.com>
2025-07-30 15:21:23 +0530

    Create min and max.cpp

---
+++ min and max.cpp
@@ -0,0 +1,9 @@
+for(int i = 1; i < size; i++) {
+        if(array[i] < min)
+            min = array[i];
+        if(array[i] > max)
+            max = array[i];
+    }
+
+    cout << "Minimum element: " << min << endl;
+    cout << "Maximum element: " << max << endl;
