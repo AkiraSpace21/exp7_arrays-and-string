8d8be94028be9c4f68d7e0d171ff1568fcf1dcbc
Jay Paygude <jaypaygude2@gmail.com>
2025-07-30 14:53:58 +0530

    Create printing array.cpp

---
+++ printing array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int array[]={1,3,4,5,6,7,8};
+    for(int i=0;i<7;i++){
+        cout<<array[i]<<" ";        
+    }
+
+    return 0;
+}
